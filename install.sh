e3d01e42ed83fb6a876ec51e370f3961186329e7
Lea MR <leandro.miranda.rios@gmail.com>
2024-07-31 20:26:43 -0300

    feat: first version of install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [[ $EUID -eq 0 ]]; then
     echo "This script should not be executed as root! Exiting......."
     exit 1
@@ -8,14 +7,14 @@ fi
 
 clear
 
-#TODO add copr repo to dnf
-#TODO install hyprland, hyprlock, hypridle, hyprpaper(or better)
-#TODO install waybar (copy default config/styles to home config folder)
-#TODO install kitty
-#TODO install zsh + oh my zsh
+sudo dnf copr enable solopasha/hyprland
+sudo dnf install hyprland hyprlock hypridle hyprpaper waybar zsh
 
-#TODO copy default config files to default paths
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#sudo dnf copr enable solopasha/hyprland
+
+cp ./config-files/. ~/.config/
+
+echo "Done!"
 
 
